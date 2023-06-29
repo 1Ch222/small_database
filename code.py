999f8324b0d712b433e91135b0054c09a3e07d36
1Ch222 <137825924+1Ch222@users.noreply.github.com>
2023-06-29 09:40:30 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,13 @@
 
+
 import os
 import json
 from collections import defaultdict
-import torch
-from torchvision import transforms
 from PIL import Image
 
 def count_pixels(image_path, json_file):
-    # Charger l'image en utilisant PyTorch
+    # Charger l'image
     image = Image.open(image_path)
-    image = transforms.ToTensor()(image)
-    image = image.cuda()  # Transférer l'image sur le GPU
 
     # Charger le fichier JSON des associations de couleur et de classe
     with open(json_file) as file:
@@ -20,11 +17,11 @@ def count_pixels(image_path, json_file):
     class_counts = defaultdict(int)
 
     # Parcourir chaque pixel de l'image
-    width, height = image.size(2), image.size(1)
+    width, height = image.size
     for x in range(width):
         for y in range(height):
-            pixel = image[:, y, x].tolist()
-            hex_color = '#{:02x}{:02x}{:02x}'.format(int(pixel[0]*255), int(pixel[1]*255), int(pixel[2]*255))
+            pixel = image.getpixel((x, y))
+            hex_color = '#{:02x}{:02x}{:02x}'.format(*pixel)
 
             # Vérifier si le code couleur a une classe associée dans le fichier JSON
             if hex_color in color_classes:
@@ -36,25 +33,23 @@ def count_pixels(image_path, json_file):
 
 
 # Parcourir le dossier contenant les fichiers PNG
-folder_path = "/home/poc2014/small_dataset/677d5238-e36b-41a4-bd38-e85a0f95f285_result/ENSTA Paris/envoie_playement/Anthony-Blr/Left"
-json_path = "t_1621535652_3.png.json"
+folder_path = "/Users/maxime.pariente/Downloads/small_dataset/semantic/677d5238-e36b-41a4-bd38-e85a0f95f285_result/ENSTA_Paris/envoie_playement/Anthony-Blr/Left"
+json_path = "/Users/maxime.pariente/Downloads/small_dataset/semantic/677d5238-e36b-41a4-bd38-e85a0f95f285_result/ENSTA_Paris/envoie_playement/Anthony-Blr/Left/t_1621535652_3.png.json"
 png_files = [file for file in os.listdir(folder_path) if file.endswith('.png')]
 
-L = []
-# Parcourir chaque fichier PNG et compter les pixels par couleur
-for png_file in png_files:
-    X = []
-    png_file_path = os.path.join(folder_path, png_file)
-    with torch.cuda.device(0):  # Utiliser le GPU pour le traitement
+# Ouvrir le fichier de sortie en mode écriture
+with open("pixel_counts.txt", 'w') as f:
+    # Parcourir chaque fichier PNG et compter les pixels par couleur
+    for png_file in png_files:
+        X = []
+        png_file_path = os.path.join(folder_path, png_file)
         result = count_pixels(png_file_path, json_path)
 
-    # Afficher le nombre de pixels par couleur pour chaque fichier PNG
-    #print(f"Fichier : {png_file}")
-    X.append(f"Fichier : {png_file}")
-    for class_name, count in result.items():
-        #print(f"{class_name}: {count} pixels")
-        X.append(f"{class_name}: {count} pixels")
-    L.append(X)
-    #print()
-    #print(L)
-print(L)
+        # Écrire le nombre de pixels par couleur dans le fichier de sortie
+        f.write(f"Fichier : {png_file}\n")
+        for class_name, count in result.items():
+            f.write(f"{class_name}: {count} pixels\n")
+        f.write('\n')
+
+print("Le traitement est terminé. Les résultats ont été enregistrés dans le fichier 'pixel_counts.txt'.")
+
